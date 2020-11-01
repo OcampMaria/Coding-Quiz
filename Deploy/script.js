2ee51682c94d3c295f588dfcf9ea34e94e732250
Maria Ocampo <mariaocampo0513@gmail.com>
2020-10-31 23:11:55 -0700

    Edited the JS file

--- Deploy/script.js
+++ Deploy/script.js
@@ -1,6 +1,8 @@
 var timeEl = document.querySelector(".time");
 var startButton = document.querySelector(".start");
 var questionContainerEl = document.querySelector(".questionContainer");
+var questionEl = document.querySelector(".question");
+var answerBtnEl = document.querySelector(".answerBtns");
 
 
 // starting time number. Timer will coundown starting at 75
@@ -8,50 +10,67 @@ var secondsLeft = 75;
 //created the function to create the countdown timer
 function setTime() {
     var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = "Time: " + secondsLeft;
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        sendMessage()
-      }
-  
-    }, 1000);
-  }
-  setTime();
-  
+        secondsLeft--;
+        timeEl.textContent = "Time: " + secondsLeft;
 
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage()
+        }
+
+    }, 1000);
+}
+setTime();
+
+var shuffledQuestions, currentQuestionsIndex
+console.log();
 // added an event listener to the start quiz button to run the function when clicked. 
 startButton.addEventListener("click", startQuiz)
 
-function startQuiz (){
-  console.log("started")
+function startQuiz() {
+    console.log("started");
 
-  // created the variables locally to make the elements dissapear after clicked. 
-  var start = document.querySelector(".start");
-  var direction = document.querySelector(".infoBox")
-
-  // make the directions and the start quiz button dissaoear after clicked. 
-  if (start.getElementsByClassName.display === "none"){
-    start.style.display = "block";
-  } else {
-    start.style.display = "none";
-  } 
-
-  if (direction.getElementsByClassName.display === "none"){
-    direction.style.display = "block";
-  } else {
-    direction.style.display = "none";
-  } 
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionsIndex = 0
+        // created the variables locally to make the elements dissapear after clicked. 
+    var start = document.querySelector(".start");
+    var direction = document.querySelector(".infoBox")
 
 
+    // make the directions and the start quiz button dissaoear after clicked. 
+    if (start.getElementsByClassName.display === "none") {
+        start.style.display = "block";
+    } else {
+        start.style.display = "none";
+    }
+    if (direction.getElementsByClassName.display === "none") {
+        direction.style.display = "block";
+    } else {
+        direction.style.display = "none";
+    }
+
+    setNextQuestion()
+}
+
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionsIndex])
 
 }
 
-function setNextQuestion (){
+function showQuestion(question) {
 
 }
 
-function selectAnswer (){
+function selectAnswer() {
 
 }
+
+
+var questions = [{
+    question: "What is JavaScript",
+    answers: [
+        { text: "A programing language", correct: true },
+        { text: "A type of animal", correct: false }
+    ]
+}];
