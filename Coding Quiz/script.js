7bd4708223e9511c01a1b6ffacbf357fffc873c5
Maria Ocampo <mariaocampo0513@gmail.com>
2020-10-31 17:59:00 -0700

    updated files. Added java and css

---
+++ Coding Quiz/script.js
@@ -0,0 +1,28 @@
+var timeEl = document.querySelector(".time");
+var quizEl = document.querySelector("#time");
+var submitBtn = document.querySelector("#submit");
+var results = document.querySelector("#results");
+
+//
+var secondsLeft = 75;
+
+
+
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = "Time: " + secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+
+    
+
+  }
+  setTime();
+  
