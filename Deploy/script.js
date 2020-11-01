e7c227a4d6a4d77a5653b8f2a8666e75f4039d73
Maria Ocampo <mariaocampo0513@gmail.com>
2020-10-31 20:46:23 -0700

    Edited the JS file. Added if statements to make elements dissapear after clicking

--- Deploy/script.js
+++ Deploy/script.js
@@ -1,5 +1,7 @@
 var timeEl = document.querySelector(".time");
 var startButton = document.querySelector(".start");
+var questionContainerEl = document.querySelector(".questionContainer");
+
 
 // starting time number. Timer will coundown starting at 75
 var secondsLeft = 75;
@@ -18,8 +20,38 @@ function setTime() {
   }
   setTime();
   
+
 // added an event listener to the start quiz button to run the function when clicked. 
 startButton.addEventListener("click", startQuiz)
+
 function startQuiz (){
   console.log("started")
+
+  // created the variables locally to make the elements dissapear after clicked. 
+  var start = document.querySelector(".start");
+  var direction = document.querySelector(".infoBox")
+
+  // make the directions and the start quiz button dissaoear after clicked. 
+  if (start.getElementsByClassName.display === "none"){
+    start.style.display = "block";
+  } else {
+    start.style.display = "none";
+  } 
+
+  if (direction.getElementsByClassName.display === "none"){
+    direction.style.display = "block";
+  } else {
+    direction.style.display = "none";
+  } 
+
+
+
+}
+
+function setNextQuestion (){
+
+}
+
+function selectAnswer (){
+
 }
